5a530b053f5cee5cdeb6bf27c97df87e6f9fd00a
Alin Peta <alin.andrei.peta@gmail.com>
2022-01-06 09:23:52 +0000

    Fixed POST /feature/create endpoint.

--- src/main/java/com/example/FeatureFlag/model/feature/FeatureCreateRequest.java
+++ src/main/java/com/example/FeatureFlag/model/feature/FeatureCreateRequest.java
@@ -3,12 +3,14 @@ package com.example.FeatureFlag.model.feature;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class FeatureCreateRequest {
 
     @NotEmpty
